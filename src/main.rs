b5700c5e67ae1881f693000729c494b5287a8826
Marcus Weber <weberma@students.uni-marburg.de>
2022-10-11 13:55:43 +0200

    surreal

--- src/main.rs
+++ src/main.rs
@@ -4,50 +4,80 @@ use reqwest::{
 };
 
 use scraper::{ElementRef, Html, Selector};
+use surrealdb::{Datastore, Session};
 
 #[tokio::main]
 async fn main() {
-    let client = reqwest::Client::new();
+    let ds = Datastore::new("file://database.db").await.unwrap();
 
-    let response = client
-        .get("https://www.mytischtennis.de/clicktt/HeTTV/22-23/verein/34031/TV-1861-Wallau/spielplan/")
-        .header(COOKIE, "valueSRV=270; iom_consent=0000000000&1661674721436; cfid=b46516ec-af15-4bfd-8e75-7042fd37d9bd; cftoken=0")
-        .header(ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        .header(ACCEPT_ENCODING, "gzip, deflate, br")
-        .header(HOST, "www.mytischtennis.de")
-        .header(REFERER, "https://www.mytischtennis.de/clicktt/HeTTV/22-23/ligen/K31-22-23/")
-        .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15")
-        .header(ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-        .send()
-        .await
-        .unwrap();
+    let ses = Session::for_kv().with_ns("testns").with_db("testdb");
+    let ast = "
+CREATE spielplan:1 SET name = 'myTT', datum = heute, heim = verein:wallau, gast = verein:kriftel;
+CREATE spielplan:2 SET name = 'myTTT', datum = morgen, heim = verein:wallau_II, gast = verein:kriftel_II;
+";
+    let res = ds.execute(ast, &ses, None, false).await.unwrap();
 
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            println!("Success!");
-        }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Need to grab a new token");
-        }
-        _ => {
-            panic!("Uh oh! Something unexpected happened.");
-        }
-    };
+    println!("{:?}", res);
 
-    let document = Html::parse_fragment(response.text().await.unwrap().as_str());
+    let qry = "
+                SELECT * FROM spielplan;
+              ";
 
-    let selector = Selector::parse(r#"table[id="playingPlanDesktop"]"#).unwrap();
-    let playing_plan_desktop: scraper::html::Select = document.select(&selector);
+    let res_qry = ds.execute(qry, &ses, None, false).await.unwrap();
 
-    let x = playing_plan_desktop.into_iter().nth(0).unwrap();
+    let x = res_qry.first().unwrap();
+    let y = x.output().unwrap().last();
 
-    let rows = extract_rows_from_table(x);
 
-    let mut backup_date = "";
-    rows.into_iter().skip(1).for_each(|row| {
-        let y = extract_column_data_from_row(row, backup_date);
-        println!("{:?}", y);
-    });
+
+
+    println!("------------");
+    println!("{:?}", y);
+    println!("{:?}", y.as_string());
+    // tx.commit().await.unwrap();
+
+    // -------------------------------------------------------------
+    // let client = reqwest::Client::new();
+    //
+    // let response = client
+    //     .get("https://www.mytischtennis.de/clicktt/HeTTV/22-23/verein/34031/TV-1861-Wallau/spielplan/")
+    //     .header(COOKIE, "valueSRV=270; iom_consent=0000000000&1661674721436; cfid=b46516ec-af15-4bfd-8e75-7042fd37d9bd; cftoken=0")
+    //     .header(ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    //     .header(ACCEPT_ENCODING, "gzip, deflate, br")
+    //     .header(HOST, "www.mytischtennis.de")
+    //     .header(REFERER, "https://www.mytischtennis.de/clicktt/HeTTV/22-23/ligen/K31-22-23/")
+    //     .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15")
+    //     .header(ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    //     .send()
+    //     .await
+    //     .unwrap();
+    //
+    // match response.status() {
+    //     reqwest::StatusCode::OK => {
+    //         println!("Success!");
+    //     }
+    //     reqwest::StatusCode::UNAUTHORIZED => {
+    //         println!("Need to grab a new token");
+    //     }
+    //     _ => {
+    //         panic!("Uh oh! Something unexpected happened.");
+    //     }
+    // };
+    //
+    // let document = Html::parse_fragment(response.text().await.unwrap().as_str());
+    //
+    // let selector = Selector::parse(r#"table[id="playingPlanDesktop"]"#).unwrap();
+    // let playing_plan_desktop: scraper::html::Select = document.select(&selector);
+    //
+    // let x = playing_plan_desktop.into_iter().nth(0).unwrap();
+    //
+    // let rows = extract_rows_from_table(x);
+    //
+    // let mut backup_date = "";
+    // rows.into_iter().skip(1).for_each(|row| {
+    //     let y = extract_column_data_from_row(row, backup_date);
+    //     println!("{:?}", y);
+    // });
 }
 
 #[derive(Debug)]
